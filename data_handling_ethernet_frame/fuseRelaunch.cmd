be49c55b4c857b4f2cfa3dfecf10d9f45c6e141c
kkeefe <kevinpk@hawaii.edu>
2019-03-11 09:53:53 -1000

    cleaning up the frames directory since most of it is not useful for this dir and is stored in SiPM git dir

--- data_handling_ethernet_frame/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ise/Documents/Projects/data_handling3/data_processing_top_isim_beh.exe" -prj "/home/ise/Documents/Projects/data_handling3/data_processing_top_beh.prj" "work.data_processing_top" "work.glbl" 

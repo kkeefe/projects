0be3c1add42b55270507821d6e29a97fc7d12816
kkeefe <kevinpk@hawaii.edu>
2019-04-25 13:13:21 -1000

    vhdl-mode indenting changes

--- workspace/process_trigger.vhd
+++ workspace/process_trigger.vhd
@@ -36,7 +36,7 @@ entity process_trigger is
     );
   Port (
     clk                : in  STD_LOGIC;
-    rst                : in  STD_LOGIC;  
+    rst                : in  STD_LOGIC;
     data_input         : in  STD_LOGIC_VECTOR(input_size-1 downto 0);
     scaler_count       : in  STD_LOGIC_VECTOR(31 downto 0);
     scaler_count_pixel : out STD_LOGIC_VECTOR(31 downto 0);
@@ -46,46 +46,47 @@ end process_trigger;
 
 architecture Behavioral of process_trigger is
 
-  constant NULL_HIT : STD_LOGIC_VECTOR(input_size - 1 downto 0)     := (others => '0');
+  constant NULL_HIT : STD_LOGIC_VECTOR(input_size - 1 downto 0) := (others => '0');
 
-  signal s_scaler_count_pixel       : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
-  signal s_current_count  				: STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+  signal s_scaler_count_pixel : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+  signal s_current_count      : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
 begin
 
 
-  process(clk) begin
+  process(clk)
+  begin
 
     if rising_edge(clk) then
-		
-		if rst = '1' then
-			new_hit 				 <= '0';
-			s_current_count    <= x"00000000";
-			scaler_count_pixel <= x"00000000";
-		else
-			--test for update count
-			if s_current_count = scaler_count then
-			  scaler_count_pixel        <= s_scaler_count_pixel;
-			  s_current_count  			 <= x"00000001"; -- setting this value to one ensures counting 'precisely'
-			  --s_scaler_count_pixel    <= x"00000000";
-			else
-				--update the signals after the process is completed..
-			  s_current_count <= std_logic_vector((unsigned(s_current_count) + 1));
-			end if;
-			
-			-- tracking for the output triggers
-			if data_input /= NULL_HIT then
-			  new_hit                   <= '1';
-			  s_scaler_count_pixel      <= std_logic_vector((unsigned(s_scaler_count_pixel) + 1));
-			--  vPrev_hit                 := data_input;
-			else
-			  new_hit <= '0';
-			end if;
+      
+      if rst = '1' then
+        new_hit            <= '0';
+        s_current_count    <= x"00000000";
+        scaler_count_pixel <= x"00000000";
+      else
+        --test for update count
+        if s_current_count = scaler_count then
+          scaler_count_pixel <= s_scaler_count_pixel;
+          s_current_count    <= x"00000001";  -- setting this value to one ensures counting 'precisely'
+        --s_scaler_count_pixel    <= x"00000000";
+        else
+          --update the signals after the process is completed..
+          s_current_count <= std_logic_vector((unsigned(s_current_count) + 1));
+        end if;
 
-       end if;  
+        -- tracking for the output triggers
+        if data_input /= NULL_HIT then
+          new_hit              <= '1';
+          s_scaler_count_pixel <= std_logic_vector((unsigned(s_scaler_count_pixel) + 1));
+        --  vPrev_hit                 := data_input;
+        else
+          new_hit <= '0';
+        end if;
+
+      end if;
     end if;
   end process;
 
-  
-end Behavioral;
+
+               end Behavioral;
 
